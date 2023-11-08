54ba86bee0a1e9f233ed96eecc104a0847701850
albertDev <116018402+aam9063@users.noreply.github.com>
2023-11-08 22:41:50 +0100

    Create Reto1.cs

---
+++ Reto1.cs
@@ -0,0 +1,89 @@
+/*
+ * ** El reto **
+Un espía está enviando mensajes encriptados.
+
+Tu misión es crear un programa que nos ayude a buscar patrones...
+
+Los mensajes son palabras separadas por espacios como este:
+gato perro perro coche Gato peRRo sol
+
+Necesitamos que el programa nos devuelva el número de veces que aparece cada palabra en el mensaje, independientemente de si está en mayúsculas o minúsculas.
+
+El resultado será una cadena de texto con la palabra y el número de veces que aparece en el mensaje, con este formato:
+gato2perro3coche1sol1
+
+¡Las palabras son ordenadas por su primera aparición en el mensaje!
+
+** Más ejemplos: **
+llaveS casa CASA casa llaves -> llaves2casa3
+taza ta za taza -> taza2ta1za1
+casas casa casasas -> casas1casa1casas1
+ */
+
+
+
+using System;
+
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string mensaje = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar " +
+            "tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario " +
+            "loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon " +
+            "murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte" +
+            " elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo " +
+            "zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra" +
+            " oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa " +
+            "leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre" +
+            " jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila " +
+            "halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache " +
+            "ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma " +
+            "pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo " +
+            "rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa " +
+            "murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino" +
+            " flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo" +
+            " oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa " +
+            "cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo " +
+            "oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache " +
+            "ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar" +
+            " oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo" +
+            " toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache " +
+            "ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon " +
+            "paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon" +
+            " murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon"; 
+
+        // Dividir el mensaje en palabras separadas por espacios y convertirlas a minúsculas
+        string[] palabras = mensaje.ToLower().Split(' ');
+
+        // Crear un diccionario para realizar un seguimiento del recuento de palabras
+        Dictionary<string, int> recuentoPalabras = new Dictionary<string, int>();
+
+        // Recorrer las palabras y contar su frecuencia
+        foreach (string palabra in palabras)
+        {
+            if (recuentoPalabras.ContainsKey(palabra))
+            {
+                // Si la palabra ya está en el diccionario, incrementar el contador
+                recuentoPalabras[palabra]++;
+            }
+            else
+            {
+                // Si la palabra no está en el diccionario, agregarla con un contador de 1
+                recuentoPalabras[palabra] = 1;
+            }
+        }
+
+        // Crear una cadena de texto con las palabras y sus recuentos sin espacios
+        string resultado = "";
+        foreach (var par in recuentoPalabras)
+        {
+            resultado += $"{par.Key}{par.Value}";
+        }
+
+        // Mostrar el resultado por consola
+        Console.WriteLine(resultado);
+    }
+}
